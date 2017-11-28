64ff561706418f2c86bb20b7500f960391c236df
Timothy Browne <timbrowne066@gmail.com>
2017-11-28 12:02:16 -0500

    setup webpack

---
+++ app/javascript/bundles/home/homeContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class HomeContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="HomeContainer">
+      <h1>HOME</h1>
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
